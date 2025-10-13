d576b510e0fcce6cc54a607d1a3559f935184996
haruhuynh2208@gmail.com <haruhuynh2208@gmail.com>
2025-10-13 12:30:35 +0700

    Bài 1

---
+++ bai-1.c
@@ -0,0 +1,13 @@
+// Tính tổng S(n) = 1 + 2 + 3 + ... + n
+// Gợi ý : Khai báo biến sum = 0, duyệt vòng lặp từ 1 tới n và cộng dồn i vào sum. Sau khi vòng lặp kết thúc thì in ra sum
+// Chú ý phải lặp xong mới in sum, không in sum trong vòng for khi chưa tính xong tổng các số.
+#include <stdio.h>
+int main () {
+    long long n;
+    scanf("%lld", &n);
+    long long sum = 0;
+    for(long long i=0;i<=n;i++) {
+        sum += i;
+    }
+    printf("%lld", sum);
+}
