9abc270a71e9b3e577058c9fce35adfb1fbee10f
haruhuynh2208@gmail.com <haruhuynh2208@gmail.com>
2025-10-14 23:36:04 +0700

    Bài 15

---
+++ bai-15.c
@@ -0,0 +1,13 @@
+// Nhập n không âm không quá 15, tính và in ra n! (n giai thừa)
+// Tính tích các ước của số tự nhiên N
+#include <stdio.h>
+int main() {
+    long long n;
+    scanf("%lld", &n);
+    long long giaithua = 1;
+    for(long long i=1;i<=n;i++){
+            giaithua *= i;
+    }
+    printf("%lld", giaithua);
+    return 0;
+}
