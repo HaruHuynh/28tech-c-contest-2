afd25cfe4e6a87be0c4969b8e1a2a30b511f2873
haruhuynh2208@gmail.com <haruhuynh2208@gmail.com>
2025-10-15 23:49:05 +0700

    Bài 21

---
+++ bai-21.c
@@ -0,0 +1,47 @@
+// Nhập N là một số nguyên không quá 100. In ra các hình tương ứng, mỗi hình cách nhau một dòng trống.?
+#include <stdio.h>
+int main () {
+    int n;
+    scanf("%d", &n);
+
+    for(int i=1;i<=n;i++) {
+        for(int j=1;j<=n;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i == 1 || i == n || j == 1 || j == n) {
+                printf("*");
+            }else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i == 1 || i == n || j == 1 || j == n) {
+                printf("*");
+            }else{
+                printf("#");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i == 1 || i == n || j == 1 || j == n){
+                printf("%d ", i);
+            }else{
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
