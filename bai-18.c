e5c172b655645496604e91767d8bd4c4ef3ded4e
haruhuynh2208@gmail.com <haruhuynh2208@gmail.com>
2025-10-15 13:25:56 +0700

    Bài 18

---
+++ bai-18.c
@@ -0,0 +1,15 @@
+// Nhập vào n nguyên. Đếm số lượng chữ số của n là số nguyên tố.
+#include <stdio.h>
+int main () {
+    long long n;
+    scanf("%lld", &n);
+    int dem = 0;
+    while(n>0){
+        int digit = n % 10;
+        if(digit == 2 || digit == 3 || digit == 5 || digit == 7) {
+            dem++;
+        }
+        n/=10;
+    }
+    printf("%d", dem);
+}
