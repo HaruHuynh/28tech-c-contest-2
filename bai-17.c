2916d2e6b77ae4eb9aefbc0782ef3c1703f5ca34
haruhuynh2208@gmail.com <haruhuynh2208@gmail.com>
2025-10-15 12:59:31 +0700

    Bài 17

---
+++ bai-17.c
@@ -0,0 +1,12 @@
+// Nhập vào n, tính tổng các chữ số của n, và in ra kết quả
+#include <stdio.h>
+int main() {
+    long long n;
+    scanf("%lld", &n);
+    long long tong = 0;
+    while(n>0){
+        tong += n%10;
+        n /= 10;
+    }
+    printf("%lld", tong);
+}
